edd387e8a34911e9690dc02015f6f6fd9c0412d1
Abolfazl <ab.iraninasab.crypto1400@gmail.com>
2024-12-29 08:10:36 +0330

    update config

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,7 +6,26 @@ require("hardhat-gas-reporter");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-    solidity: "0.8.24",
+    solidity: {
+        defaultNetwork: "localhost",
+        networks:{
+            localhost: {
+                url: "http://127.0.0.1:8545/",
+                // mining: {
+                //     auto: false,
+                //     interval: 0
+                // },
+                // blockGasLimit: 300000000
+            }
+        },
+        version: "0.8.19",
+        settings: {
+            // optimizer: {
+            //   enabled: true,
+            //   runs: 2000
+            // }
+        }
+    },
     gasReporter: {
         enabled: (process.env.REPORT_GAS) ? true : false
     }
