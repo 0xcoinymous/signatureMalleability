80018f164ac7360558239e1bb71f12012b5782bf
0xcoinymous <0xcoinymous@gmail.com>
2025-02-05 20:27:02 +0330

    malleableSignature script added

---
+++ scripts/malleableSignature.js
@@ -0,0 +1,33 @@
+const {Web3} = require("web3");
+const BN = require("bn.js");
+const { assert } = require("chai");
+
+const web3 = new Web3(Web3.givenProvider);
+
+const N = new BN("0xfffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141".substr(2), 16);
+
+
+function Main(){
+    let acc = web3.eth.accounts.create();
+    console.log(`Original Signer Address : ${acc.address}`);
+
+    const msgHash = web3.utils.soliditySha3("hello");
+    let sig = acc.sign(msgHash);
+
+    assert(acc.address == web3.eth.accounts.recover(msgHash, sig.signature), "Something is wrong w the signature");
+
+    let S = new BN(sig.s.substr(2), 16)
+    let newV = new BN(sig.v.substr(2), 16).mod(new BN('2')) == 0 ? new BN('1b', 16) : new BN('1c', 16);
+
+    let newSig = sig.r + 
+        N.sub(S).toString(16) + // manipulate S
+        newV.toString(16);
+
+    let recoveredSignerFromNewSignature = web3.eth.accounts.recover(msgHash, newSig);
+    console.log(`Recovered Signer Address : ${acc.address}`);
+
+    assert(recoveredSignerFromNewSignature == acc.address, "Signers are not equal");
+    console.log("Signature Malleability Attack Done.")
+}
+
+Main()
